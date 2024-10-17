95fa661717fbf900d0348994f1ffe460bb03eea7
rafael-f-dev <rafael.freire.developer@gmail.com>
2024-10-17 15:42:54 +0100

    continuing

--- vintage-vault-client/src/App.js
+++ vintage-vault-client/src/App.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar.js';
 import Home from './views/Home.js';
 import Login from './views/Login.js';
 import Register from './views/Register.js';
 import Profile from './views/Profile.js';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 
 function App() {
@@ -24,13 +24,12 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path ="/" render={Home}/>
-          <Route path ="/login" render={(props) => isLoggedIn ? <Navigate to="/"/> : <Login login={login} />}/>
-          <Route path ="/register" render={(props) => isLoggedIn ? <Navigate to="/"/> : <Register login={login} />}/>
-          <Route path ="/profile" render={(props) => isLoggedIn ? <Profile logout={logout} /> : <Navigate to="/"/>}/>
+          <Route path ="/" element={<Home/>}/>
+          <Route path ="/login" element={isLoggedIn ? <Navigate to="/"/> : <Login login={login} />}/>
+          <Route path ="/register" element={isLoggedIn ? <Navigate to="/"/> : <Register login={login} />}/>
+          <Route path ="/profile" element={isLoggedIn ? <Profile logout={logout} /> : <Navigate to="/"/>}/>
         </Routes>
       </Router>
-      
     </div>
   );
 }
--- vintage-vault-client/src/components/Navbar.js
+++ vintage-vault-client/src/components/Navbar.js
@@ -5,10 +5,10 @@ const Navbar = () => {
 
     return (
         <div className='navbar'>
-            <NavLink to="/" style={styles.default} activeStyle={styles.active} >Home</NavLink>
-            <NavLink to="/login" style={styles.default} activeStyle={styles.active} >Login</NavLink>
-            <NavLink to="/register" style={styles.default} activeStyle={styles.active} >Register</NavLink>
-            <NavLink to="/profile" style={styles.default} activeStyle={styles.active} >Profile</NavLink>
+            <NavLink to={"/"} style={styles.default} activeStyle={styles.active} >Home</NavLink>
+            <NavLink to={"/login"} style={styles.default} activeStyle={styles.active} >Login</NavLink>
+            <NavLink to={"/register"} style={styles.default} activeStyle={styles.active} >Register</NavLink>
+            <NavLink to={"/profile"} style={styles.default} activeStyle={styles.active} >Profile</NavLink>
         </div>
     )
 }
