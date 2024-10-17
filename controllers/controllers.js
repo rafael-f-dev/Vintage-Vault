0f4c63d2a013ba44197fdfb736d9e074a704bdfe
rafael-f-dev <rafael.freire.developer@gmail.com>
2024-10-17 13:36:24 +0100

    continuing

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,10 +1,41 @@
-const products = require('../models/products.js')
+const Products = require('../models/products.js')
 
 
 class Product {
 
+   async findAll(req,res) {
+       try{
+           const products = await Products.find({});
+           res.send(products);
+       }
+       catch(e){
+           res.send({e})
+       }
+    }
 
-	
+   async insert (req, res) {
+        let { productname, productprice, productdesc, productcat } = req.body;
+        try{
+            const done = await Products.create({name:productname,price:productprice,description:productdesc,category:productcat});
+            res.send(done)
+        }
+        catch(e){
+            res.send({e})
+        }
+    }
+
+    async delete (req, res) {
+        let { product } = req.body;
+        try{
+            const removed = await Products.deleteOne({name:product});
+            res.send({removed})
+        }
+        catch(e){
+            res.send({e})
+        }
+    }
 }
+	
+
 
 module.exports = new Product();
--- routes/routes.js
+++ routes/routes.js
@@ -2,4 +2,11 @@ const express = require('express'),
   router      = express.Router(),
   controller  = require('../controllers/controllers.js');
 
+
+router.get('/', controller.findAll);
+
+router.post('/new', controller.insert);
+
+router.post('/delete', controller.delete);
+
 module.exports = router
