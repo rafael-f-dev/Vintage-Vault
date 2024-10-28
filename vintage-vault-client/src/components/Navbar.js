1f438419ef4621d24a231a21da0987bee1362378
rafael-f-dev <rafael.freire.developer@gmail.com>
2024-10-28 12:29:17 +0000

    made navbar responsive

--- vintage-vault-client/src/components/Navbar.js
+++ vintage-vault-client/src/components/Navbar.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Navbar = (props) => {
 
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth > 800 && menuOpen) {
+                setMenuOpen(false);
+            }
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    },[menuOpen]);
+
     return (
+        <div>
         <div className='navbar'>
             <NavLink to={"/"} className={({ isActive }) => (isActive ? 'active-logo' : 'default-logo')} >Vintage Vault.</NavLink>
-            <NavLink to={"/products"} className={({ isActive }) => (isActive ? 'active' : 'default')} >Products</NavLink>
-            <NavLink to={"/login"} className={({ isActive }) => (isActive ? 'active' : 'default')} >Login</NavLink>
-            <NavLink to={"/profile"} className={({ isActive }) => (isActive ? 'active' : 'default')} >Profile</NavLink>
-            <NavLink to={"/cart"} className={({ isActive }) => (isActive ? 'active' : 'default')} >Cart ({props.cartCount})</NavLink>
+            <div className='menu' onClick={() => setMenuOpen(!menuOpen)}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <div className={menuOpen ? "nav-links-open" : "nav-links"}>
+            <NavLink to={"/products"} onClick={() => setMenuOpen(false)} className={({ isActive }) => (isActive ? 'active' : 'default')} >Products</NavLink>
+            <NavLink to={"/login"} onClick={() => setMenuOpen(false)} className={({ isActive }) => (isActive ? 'active' : 'default')} >Login</NavLink>
+            <NavLink to={"/profile"} onClick={() => setMenuOpen(false)} className={({ isActive }) => (isActive ? 'active' : 'default')} >Profile</NavLink>
+            <NavLink to={"/cart"} onClick={() => setMenuOpen(false)} className={({ isActive }) => (isActive ? 'active' : 'default')} >Cart ({props.cartCount})</NavLink>
+            </div>
+        </div>
         </div>
     )
 }
